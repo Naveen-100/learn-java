35339ada002bae1429a8472a8afd82746d6267d0
Naveen <kalidindi.naveen.2000@gmail.com>
2023-04-17 10:45:12 +0530

    M1

---
+++ Miscellaneous/Var.java
@@ -0,0 +1,20 @@
+package Miscellaneous;
+
+public class Var {
+  public static void main(String[] args) {
+    int $ = 55;
+    System.out.println($);
+
+    int $count = 55;  
+    System.out.println($count);
+
+    // int 1count = 55;  // variable name can't start with number
+    // System.out.println(1count);
+
+    // int _ = 55; Error
+    // System.out.println(_);
+
+    int _count = 55;
+    System.out.println(_count);
+  }
+}
